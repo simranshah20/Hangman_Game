013c1fdb45543a04ef67e334c3a2a3ffe92a9fe6
Simu <122908940+simranshah20@users.noreply.github.com>
2024-11-02 15:57:50 +0530

    Update TextInput.jsx

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,14 +1,16 @@
-function TextInput({ type="text",label,placeholder="Enter your input here....",onChangeHandler}){ //here type is a props
+const TextInput =({ type="text",label,placeholder="Enter your input here....",onChangeHandler}) =>{ //here type is a props
 return(
+    <div>
     <label>
         <span className="text-gray-700">{label}</span>
+        </label>
         <input
         type={type}  //{type} is  a prop
         className="px-4 py-2 border border-gray-600 rounded-md w-full" //w-full mean 100% width of its parent width
         placeholder={placeholder} //{placeholder} is a prop
         onChange={onChangeHandler} //{onChangedHandler} is a prop
         />
-    </label>
+    </div>
 )
 }
 export default TextInput;
